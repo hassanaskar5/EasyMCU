3519a276dab9abbcf333e2af3f5259d9b23d5a78
askar <askar@hassanaskar>
2020-12-10 11:34:31 +0200

    fixing some bug

---
+++ NetBeansProjects/Easy-ATMega32-master/src/main/java/com/hassan/askar/easyatmega32/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hassan.askar.easyatmega32;
+
+/**
+ * Main method
+ *
+ * @author Askar
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        EasyATMega32 run = new EasyATMega32();
+        run.setVisible(true);
+        //xfhkzfh
+
+
+    }
+}
